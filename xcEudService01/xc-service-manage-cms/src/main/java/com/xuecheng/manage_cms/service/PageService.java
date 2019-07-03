1141c692095e1722970c8d1813189c16e06f9e7f
lyz8jj0 <844216765@qq.com>
2019-07-03 18:26:45 +0800

    自定义查询测试和服务器开发完成,准备编写自定义前端查询页面

--- xcEudService01/xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/service/PageService.java
+++ xcEudService01/xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/service/PageService.java
@@ -6,10 +6,9 @@ import com.xuecheng.framework.model.response.CommonCode;
 import com.xuecheng.framework.model.response.QueryResponseResult;
 import com.xuecheng.framework.model.response.QueryResult;
 import com.xuecheng.manage_cms.dao.CmsPageRepository;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.*;
 import org.springframework.stereotype.Service;
 
 /**
@@ -26,27 +25,49 @@ public class PageService {
 
     /**
      * 页面查询方法
-     * @param page 页码，从1开始记数
-     * @param size 每页记录数
+     *
+     * @param page             页码，从1开始记数
+     * @param size             每页记录数
      * @param queryPageRequest 查询条件
      * @return
      */
-    public QueryResponseResult findList(int page, int size, QueryPageRequest queryPageRequest){
-
+    public QueryResponseResult findList(int page, int size, QueryPageRequest queryPageRequest) {
+        if (queryPageRequest == null) {
+            queryPageRequest = new QueryPageRequest();
+        }
+        //自定义条件查询
+        //定义条件匹配器
+        ExampleMatcher exampleMatcher = ExampleMatcher.matching()
+                .withMatcher("pageAlise", ExampleMatcher.GenericPropertyMatchers.contains());
+        //条件值对象
+        CmsPage cmsPage = new CmsPage();
+        //设置条件值(站点id)
+        if (StringUtils.isNoneEmpty(queryPageRequest.getSiteId())) {
+            cmsPage.setSiteId(queryPageRequest.getSiteId());
+        }
+        //设置模板id作为查询条件
+        if (StringUtils.isNoneEmpty(queryPageRequest.getTemplateId())) {
+            cmsPage.setTemplateId(queryPageRequest.getTemplateId());
+        }
+        //设置页面另别名作为查询条件
+        if (StringUtils.isNoneEmpty(queryPageRequest.getPageAliase())) {
+            cmsPage.setPageAliase(queryPageRequest.getPageAliase());
+        }
+        Example<CmsPage> example = Example.of(cmsPage, exampleMatcher);
         //分页参数
-        if(page <=0){
+        if (page <= 0) {
             page = 1;
         }
-        page = page -1;
-        if(size<=0){
+        page = page - 1;
+        if (size <= 0) {
             size = 10;
         }
-        Pageable pageable = PageRequest.of(page,size);
-        Page<CmsPage> all = cmsPageRepository.findAll(pageable);
+        Pageable pageable = PageRequest.of(page, size);
+        Page<CmsPage> all = cmsPageRepository.findAll(example,pageable);//自定义条件查询并分页查询
         QueryResult queryResult = new QueryResult();
         queryResult.setList(all.getContent());//数据列表
         queryResult.setTotal(all.getTotalElements());//数据总记录数
-        QueryResponseResult queryResponseResult = new QueryResponseResult(CommonCode.SUCCESS,queryResult);
+        QueryResponseResult queryResponseResult = new QueryResponseResult(CommonCode.SUCCESS, queryResult);
         return queryResponseResult;
     }
 }
--- xcEudService01/xc-service-manage-cms/src/test/java/com/xuecheng/manage_cms/dao/CmsPageRepositoryTest.java
+++ xcEudService01/xc-service-manage-cms/src/test/java/com/xuecheng/manage_cms/dao/CmsPageRepositoryTest.java
@@ -5,9 +5,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.*;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
@@ -26,7 +24,7 @@ public class CmsPageRepositoryTest {
     CmsPageRepository cmsPageRepository;
 
     @Test
-    public void testFindAll(){
+    public void testFindAll() {
         List<CmsPage> all = cmsPageRepository.findAll();
         System.out.println(all);
 
@@ -34,11 +32,11 @@ public class CmsPageRepositoryTest {
 
     //分页查询
     @Test
-    public void testFindPage(){
+    public void testFindPage() {
         //分页参数
         int page = 1;//从0开始
         int size = 10;
-        Pageable pageable = PageRequest.of(page,size);
+        Pageable pageable = PageRequest.of(page, size);
         Page<CmsPage> all = cmsPageRepository.findAll(pageable);
         System.out.println(all);
     }
@@ -48,7 +46,7 @@ public class CmsPageRepositoryTest {
     public void testUpdate() {
         //查询对象
         Optional<CmsPage> optional = cmsPageRepository.findById("5b4b1d8bf73c6623b03f8cec");
-        if(optional.isPresent()){
+        if (optional.isPresent()) {
             CmsPage cmsPage = optional.get();
             //设置要修改值
             cmsPage.setPageAliase("test01");
@@ -62,8 +60,31 @@ public class CmsPageRepositoryTest {
 
     //根据页面名称查询
     @Test
-    public void testfindByPageName(){
+    public void testfindByPageName() {
         CmsPage cmsPage = cmsPageRepository.findByPageName("测试页面");
         System.out.println(cmsPage);
+    }
+
+    @Test
+    public void testFindAllByExample() {
+        //分页参数
+        int page = 0; //从0开始
+        int size = 10;
+        Pageable pageable = PageRequest.of(page, size);
+
+        //条件值对象
+        CmsPage cmsPage = new CmsPage();
+        //要查询5a751fab6abb5044e0d19ea1站点的页面
+//        cmsPage.setSiteId("5a751fab6abb5044e0d19ea1");
+        //设置网页别名
+        cmsPage.setPageAliase("轮播");
+        //条件匹配器
+        ExampleMatcher exampleMatcher = ExampleMatcher.matching();
+        exampleMatcher = exampleMatcher.withMatcher("pageAliase", ExampleMatcher.GenericPropertyMatchers.contains());
+        //定义Example
+        Example<CmsPage> example = Example.of(cmsPage, exampleMatcher);
+        Page<CmsPage> all = cmsPageRepository.findAll(example, pageable);
+        List<CmsPage> content = all.getContent();
+        System.out.println(content);
     }
 }
