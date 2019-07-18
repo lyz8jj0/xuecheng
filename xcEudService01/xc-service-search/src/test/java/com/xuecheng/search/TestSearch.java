64b25d17ee880629d0d121b8c48f344a8aa02b0d
lyz8jj0 <844216765@qq.com>
2019-07-18 16:55:23 +0800

    DSL搜索-布尔查询和过滤器查询test测试

--- xcEudService01/xc-service-search/src/test/java/com/xuecheng/search/TestSearch.java
+++ xcEudService01/xc-service-search/src/test/java/com/xuecheng/search/TestSearch.java
@@ -4,8 +4,7 @@ import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
-import org.elasticsearch.index.query.QueryBuilder;
-import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.query.*;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
@@ -145,7 +144,189 @@ public class TestSearch {
         searchSourceBuilder.from(from);//起始记录下标,从0开始
         searchSourceBuilder.size(size);//第页显示的记录数
         //搜索方式
-        searchSourceBuilder.query(QueryBuilders.termQuery("name","spring"));
+        searchSourceBuilder.query(QueryBuilders.termQuery("name", "spring"));
+        //设置源字段过滤,第一个参数结果集包括哪些字段, 第二个参数表示结果集不包括哪些字段
+        searchSourceBuilder.fetchSource(new String[]{"name", "studymodel", "price", "timestamp"}, new String[]{});
+        //向搜索请求对象中设置搜索源
+        searchRequest.source(searchSourceBuilder);
+        //执行搜索
+        SearchResponse searchResponse = client.search(searchRequest);
+        //搜索结果
+        SearchHits hits = searchResponse.getHits();
+        //匹配到的总记录数
+        long totalHits = hits.getTotalHits();
+        //得到匹配度高的文档
+        SearchHit[] searchHits = hits.getHits();
+        //日期格式化对象
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        for (SearchHit hit : searchHits) {
+            //文档的主键
+            String id = hit.getId();
+            //源文档内容
+            Map<String, Object> sourceAsMap = hit.getSourceAsMap();
+            String name = (String) sourceAsMap.get("name");
+            //学习模式
+            String studymodel = (String) sourceAsMap.get("studymodel");
+            //价格
+            Double price = (Double) sourceAsMap.get("price");
+            //日期
+            Date timestamp = dateFormat.parse((String) sourceAsMap.get("timestamp"));
+        }
+    }
+
+
+    //multiMatchQuery
+    @Test
+    public void testMultiMatchQuery() throws IOException, ParseException {
+        //索引请求对象
+        SearchRequest searchRequest = new SearchRequest("xc_course");
+        //指定类型
+        searchRequest.types("doc");
+        //搜索源构建对象
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        //设置分布参数
+        //页码
+        int page = 1;
+        //每页记录数
+        int size = 1;
+        //计算出记录起始下标
+        int from = (page - 1) * size;
+        searchSourceBuilder.from(from);//起始记录下标,从0开始
+        searchSourceBuilder.size(size);//第页显示的记录数
+        //搜索方式
+        searchSourceBuilder.query(QueryBuilders.multiMatchQuery("spring css", "name", "description")
+                .minimumShouldMatch("50%")
+                .field("name", 10));
+
+        //设置源字段过滤,第一个参数结果集包括哪些字段, 第二个参数表示结果集不包括哪些字段
+        searchSourceBuilder.fetchSource(new String[]{"name", "studymodel", "price", "timestamp"}, new String[]{});
+        //向搜索请求对象中设置搜索源
+        searchRequest.source(searchSourceBuilder);
+        //执行搜索
+        SearchResponse searchResponse = client.search(searchRequest);
+        //搜索结果
+        SearchHits hits = searchResponse.getHits();
+        //匹配到的总记录数
+        long totalHits = hits.getTotalHits();
+        //得到匹配度高的文档
+        SearchHit[] searchHits = hits.getHits();
+        //日期格式化对象
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        for (SearchHit hit : searchHits) {
+            //文档的主键
+            String id = hit.getId();
+            //源文档内容
+            Map<String, Object> sourceAsMap = hit.getSourceAsMap();
+            String name = (String) sourceAsMap.get("name");
+            //学习模式
+            String studymodel = (String) sourceAsMap.get("studymodel");
+            //价格
+            Double price = (Double) sourceAsMap.get("price");
+            //日期
+            Date timestamp = dateFormat.parse((String) sourceAsMap.get("timestamp"));
+        }
+    }
+
+
+    //BoolQuery
+    @Test
+    public void testBoolQuery() throws IOException, ParseException {
+        //索引请求对象
+        SearchRequest searchRequest = new SearchRequest("xc_course");
+        //指定类型
+        searchRequest.types("doc");
+        //搜索源构建对象
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        //设置分布参数
+        //页码
+        int page = 1;
+        //每页记录数
+        int size = 1;
+        //计算出记录起始下标
+        int from = (page - 1) * size;
+        searchSourceBuilder.from(from);//起始记录下标,从0开始
+        searchSourceBuilder.size(size);//第页显示的记录数
+
+        //boolQuery搜索方式
+        //先定义一个MultiMatchQuery
+        MultiMatchQueryBuilder multiMatchQueryBuilder = QueryBuilders.multiMatchQuery("spring css", "name", "description")
+                .minimumShouldMatch("50%")
+                .field("name", 10);
+        //再定义 一个termQuery
+        TermQueryBuilder termQueryBuilder = QueryBuilders.termQuery("studymodel", "201001");
+
+        //定义一个boolQuery
+        BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
+        boolQueryBuilder.must(multiMatchQueryBuilder);
+        boolQueryBuilder.must(termQueryBuilder);
+
+        searchSourceBuilder.query(boolQueryBuilder);
+
+        //设置源字段过滤,第一个参数结果集包括哪些字段, 第二个参数表示结果集不包括哪些字段
+        searchSourceBuilder.fetchSource(new String[]{"name", "studymodel", "price", "timestamp"}, new String[]{});
+        //向搜索请求对象中设置搜索源
+        searchRequest.source(searchSourceBuilder);
+        //执行搜索
+        SearchResponse searchResponse = client.search(searchRequest);
+        //搜索结果
+        SearchHits hits = searchResponse.getHits();
+        //匹配到的总记录数
+        long totalHits = hits.getTotalHits();
+        //得到匹配度高的文档
+        SearchHit[] searchHits = hits.getHits();
+        //日期格式化对象
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        for (SearchHit hit : searchHits) {
+            //文档的主键
+            String id = hit.getId();
+            //源文档内容
+            Map<String, Object> sourceAsMap = hit.getSourceAsMap();
+            String name = (String) sourceAsMap.get("name");
+            //学习模式
+            String studymodel = (String) sourceAsMap.get("studymodel");
+            //价格
+            Double price = (Double) sourceAsMap.get("price");
+            //日期
+            Date timestamp = dateFormat.parse((String) sourceAsMap.get("timestamp"));
+        }
+    }
+
+
+
+    //filter
+    @Test
+    public void testFilter() throws IOException, ParseException {
+        //索引请求对象
+        SearchRequest searchRequest = new SearchRequest("xc_course");
+        //指定类型
+        searchRequest.types("doc");
+        //搜索源构建对象
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        //设置分布参数
+        //页码
+        int page = 1;
+        //每页记录数
+        int size = 1;
+        //计算出记录起始下标
+        int from = (page - 1) * size;
+        searchSourceBuilder.from(from);//起始记录下标,从0开始
+        searchSourceBuilder.size(size);//第页显示的记录数
+
+        //boolQuery搜索方式
+        //先定义一个MultiMatchQuery
+        MultiMatchQueryBuilder multiMatchQueryBuilder = QueryBuilders.multiMatchQuery("spring css", "name", "description")
+                .minimumShouldMatch("50%")
+                .field("name", 10);
+
+        //定义一个boolQuery
+        BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
+        boolQueryBuilder.must(multiMatchQueryBuilder);
+        //定义一个过滤器
+        boolQueryBuilder.filter(QueryBuilders.termQuery("studymodel","201001"));
+        boolQueryBuilder.filter(QueryBuilders.rangeQuery("price").gte(10).lte(100));
+
+        searchSourceBuilder.query(boolQueryBuilder);
+
         //设置源字段过滤,第一个参数结果集包括哪些字段, 第二个参数表示结果集不包括哪些字段
         searchSourceBuilder.fetchSource(new String[]{"name", "studymodel", "price", "timestamp"}, new String[]{});
         //向搜索请求对象中设置搜索源
