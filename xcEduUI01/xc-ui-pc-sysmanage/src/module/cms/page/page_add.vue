cb05fe56269b6a58179135f724fa3c7138c481a3
lyz8jj0 <844216765@qq.com>
2019-07-18 10:51:47 +0800

    编写页面静态部分, 即view部分

--- xcEduUI01/xc-ui-pc-sysmanage/src/module/cms/page/page_add.vue
+++ xcEduUI01/xc-ui-pc-sysmanage/src/module/cms/page/page_add.vue
@@ -1,50 +1,6 @@
 <template>
   <!--编写页面静态部分, 即view部分-->
-  <div>
-    <!--查询表单-->
-    <el-form :model="params">
-      <el-select v-model="params.siteId" placeholder="请选择站点">
-        <el-option
-          v-for="item in siteList"
-          :key="item.siteId"
-          :label="item.siteName"
-          :value="item.siteId">
-        </el-option>
-      </el-select>
-      页面别名：
-      <el-input v-model="params.pageAliase" style="width: 100px"></el-input>
-      <el-button type="primary" v-on:click="query" size="small">查询</el-button>
-    </el-form>
-
-
-    <el-table
-      :data="list"
-      stripe
-      style="width: 100%">
-      <el-table-column type="index" width="60">
-      </el-table-column>
-      <el-table-column prop="pageName" label="页面名称" width="120">
-      </el-table-column>
-      <el-table-column prop="pageAliase" label="别名" width="120">
-      </el-table-column>
-      <el-table-column prop="pageType" label="页面类型" width="150">
-      </el-table-column>
-      <el-table-column prop="pageWebPath" label="访问路径" width="250">
-      </el-table-column>
-      <el-table-column prop="pagePhysicalPath" label="物理路径" width="250">
-      </el-table-column>
-      <el-table-column prop="pageCreateTime" label="创建时间" width="180">
-      </el-table-column>
-    </el-table>
-    <el-pagination
-      layout="prev, pager, next"
-      :total="total"
-      :page-size="params.size"
-      :current-page="params.page"
-      @current-change="changePage"
-      style="float:right">
-    </el-pagination>
-  </div>
+  <div></div>
 </template>
 <script>
   // 编写页面静态部分,即model及vm部分
