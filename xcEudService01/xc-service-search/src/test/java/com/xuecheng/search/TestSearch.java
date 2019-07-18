33d632bcbd0288af68328c70ddd9d640c26f9c0f
lyz8jj0 <844216765@qq.com>
2019-07-18 18:03:09 +0800

    DSL搜索-排序和高亮test测试

--- xcEudService01/xc-service-search/src/test/java/com/xuecheng/search/TestSearch.java
+++ xcEudService01/xc-service-search/src/test/java/com/xuecheng/search/TestSearch.java
@@ -4,10 +4,14 @@ import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.text.Text;
 import org.elasticsearch.index.query.*;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightField;
+import org.elasticsearch.search.sort.SortOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +19,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.naming.directory.SearchResult;
+import javax.swing.text.Highlighter;
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.ParseException;
@@ -292,7 +297,6 @@ public class TestSearch {
     }
 
 
-
     //filter
     @Test
     public void testFilter() throws IOException, ParseException {
@@ -322,7 +326,7 @@ public class TestSearch {
         BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
         boolQueryBuilder.must(multiMatchQueryBuilder);
         //定义一个过滤器
-        boolQueryBuilder.filter(QueryBuilders.termQuery("studymodel","201001"));
+        boolQueryBuilder.filter(QueryBuilders.termQuery("studymodel", "201001"));
         boolQueryBuilder.filter(QueryBuilders.rangeQuery("price").gte(10).lte(100));
 
         searchSourceBuilder.query(boolQueryBuilder);
@@ -347,6 +351,150 @@ public class TestSearch {
             //源文档内容
             Map<String, Object> sourceAsMap = hit.getSourceAsMap();
             String name = (String) sourceAsMap.get("name");
+            //学习模式
+            String studymodel = (String) sourceAsMap.get("studymodel");
+            //价格
+            Double price = (Double) sourceAsMap.get("price");
+            //日期
+            Date timestamp = dateFormat.parse((String) sourceAsMap.get("timestamp"));
+        }
+    }
+
+    //Sort
+    @Test
+    public void testSort() throws IOException, ParseException {
+        //索引请求对象
+        SearchRequest searchRequest = new SearchRequest("xc_course");
+        //指定类型
+        searchRequest.types("doc");
+        //搜索源构建对象
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        //设置分布参数
+        //页码
+        int page = 1;
+        //每页记录数
+        int size = 1;
+        //计算出记录起始下标
+        int from = (page - 1) * size;
+        searchSourceBuilder.from(from);//起始记录下标,从0开始
+        searchSourceBuilder.size(size);//第页显示的记录数
+
+        //boolQuery搜索方式
+        //定义一个boolQuery
+        BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
+        //定义一个过滤器
+        boolQueryBuilder.filter(QueryBuilders.rangeQuery("price").gte(0).lte(100));
+
+        searchSourceBuilder.query(boolQueryBuilder);
+        //添加排序
+        searchSourceBuilder.sort("studymodel", SortOrder.DESC);
+        searchSourceBuilder.sort("price", SortOrder.ASC);
+
+        //设置源字段过滤,第一个参数结果集包括哪些字段, 第二个参数表示结果集不包括哪些字段
+        searchSourceBuilder.fetchSource(new String[]{"name", "studymodel", "price", "timestamp"}, new String[]{});
+        //向搜索请求对象中设置搜索源
+        searchRequest.source(searchSourceBuilder);
+        //执行搜索
+        SearchResponse searchResponse = client.search(searchRequest);
+        //搜索结果
+        SearchHits hits = searchResponse.getHits();
+        //匹配到的总记录数
+        long totalHits = hits.getTotalHits();
+        //得到匹配度高的文档
+        SearchHit[] searchHits = hits.getHits();
+        //日期格式化对象
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        for (SearchHit hit : searchHits) {
+            //文档的主键
+            String id = hit.getId();
+            //源文档内容
+            Map<String, Object> sourceAsMap = hit.getSourceAsMap();
+            String name = (String) sourceAsMap.get("name");
+            //学习模式
+            String studymodel = (String) sourceAsMap.get("studymodel");
+            //价格
+            Double price = (Double) sourceAsMap.get("price");
+            //日期
+            Date timestamp = dateFormat.parse((String) sourceAsMap.get("timestamp"));
+        }
+    }
+
+    //Highlight
+    @Test
+    public void testHighlight() throws IOException, ParseException {
+        //索引请求对象
+        SearchRequest searchRequest = new SearchRequest("xc_course");
+        //指定类型
+        searchRequest.types("doc");
+        //搜索源构建对象
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        //设置分布参数
+        //页码
+        int page = 1;
+        //每页记录数
+        int size = 1;
+        //计算出记录起始下标
+        int from = (page - 1) * size;
+        searchSourceBuilder.from(from);//起始记录下标,从0开始
+        searchSourceBuilder.size(size);//第页显示的记录数
+
+        //boolQuery搜索方式
+        //先定义一个MultiMatchQuery
+        MultiMatchQueryBuilder multiMatchQueryBuilder = QueryBuilders.multiMatchQuery("开发框架", "name", "description")
+                .minimumShouldMatch("50%")
+                .field("name", 10);
+
+        //定义一个boolQuery
+        BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
+        boolQueryBuilder.must(multiMatchQueryBuilder);
+        //定义一个过滤器
+        boolQueryBuilder.filter(QueryBuilders.termQuery("studymodel", "201001"));
+        boolQueryBuilder.filter(QueryBuilders.rangeQuery("price").gte(0).lte(100));
+
+        //设置高亮
+        HighlightBuilder highlightBuilder = new HighlightBuilder();
+        highlightBuilder.preTags("<tag>");
+        highlightBuilder.postTags("</tag>");
+        highlightBuilder.fields().add(new HighlightBuilder.Field("name"));
+        searchSourceBuilder.highlighter(highlightBuilder);
+
+        searchSourceBuilder.query(boolQueryBuilder);
+
+        //设置源字段过滤,第一个参数结果集包括哪些字段, 第二个参数表示结果集不包括哪些字段
+        searchSourceBuilder.fetchSource(new String[]{"name", "studymodel", "price", "timestamp"}, new String[]{});
+        //向搜索请求对象中设置搜索源
+        searchRequest.source(searchSourceBuilder);
+        //执行搜索
+        SearchResponse searchResponse = client.search(searchRequest);
+        //搜索结果
+        SearchHits hits = searchResponse.getHits();
+        //匹配到的总记录数
+        long totalHits = hits.getTotalHits();
+        //得到匹配度高的文档
+        SearchHit[] searchHits = hits.getHits();
+        //日期格式化对象
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        for (SearchHit hit : searchHits) {
+            //文档的主键
+            String id = hit.getId();
+            //源文档内容
+            Map<String, Object> sourceAsMap = hit.getSourceAsMap();
+            //源文档的name字段
+            String name = (String) sourceAsMap.get("name");
+            //取出高亮字段
+            Map<String, HighlightField> highlightFields = hit.getHighlightFields();
+            if (highlightFields != null) {
+                HighlightField nameHighlightField = highlightFields.get("name");
+                if (nameHighlightField != null) {
+                    Text[] fragments = nameHighlightField.getFragments();
+                    StringBuilder stringBuilder = new StringBuilder();
+                    for (Text text : fragments) {
+                        stringBuilder.append(text);
+                    }
+                    name = stringBuilder.toString();
+                }
+            }
+
             //学习模式
             String studymodel = (String) sourceAsMap.get("studymodel");
             //价格
