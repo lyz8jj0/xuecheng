9443e7cc0aafea20ed9376d06042f11670c00e28
lyz8jj0 <844216765@qq.com>
2019-07-18 14:32:29 +0800

    文档和索引库客户端操作测试

---
+++ xcEudService01/xc-service-search/src/test/java/com/xuecheng/search/TestSearch.java
@@ -0,0 +1,29 @@
+package com.xuecheng.search;
+
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by 李新宇
+ * 2019-07-18 14:40
+ */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class TestSearch {
+
+    @Autowired
+    RestHighLevelClient client;
+    @Autowired
+    RestClient restClient;
+
+    //搜索全部记录
+    @Test
+    public void testSearchAll(){
+
+    }
+}
