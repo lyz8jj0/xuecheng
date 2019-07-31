909c7ba9d1df09323d3bac07caceded8d4d39e1e
lyz8jj0 <844216765@qq.com>
2019-07-31 16:50:53 +0800

    xc-ui-pc-sysmange页面从新上传

--- xcEduUI01/xc-ui-pc-sysmanage/src/module/cms/page/page_list.vue
+++ xcEduUI01/xc-ui-pc-sysmanage/src/module/cms/page/page_list.vue
@@ -1,5 +1,4 @@
 <template>
-  <!--编写页面静态部分, 即view部分-->
   <div>
     <!--查询表单-->
     <el-form :model="params">
@@ -14,8 +13,13 @@
       页面别名：
       <el-input v-model="params.pageAliase" style="width: 100px"></el-input>
       <el-button type="primary" v-on:click="query" size="small">查询</el-button>
+      <router-link :to="{path:'/cms/page/add',query:{
+        page:this.params.page,
+        siteId:this.params.siteId
+      }}">
+        <el-button type="primary" size="small">新增页面</el-button>
+      </router-link>
     </el-form>
-
 
     <el-table
       :data="list"
@@ -23,61 +27,96 @@
       style="width: 100%">
       <el-table-column type="index" width="60">
       </el-table-column>
-      <el-table-column prop="pageName" label="页面名称" width="120">
+      <el-table-column prop="pageName" label="页面名称" width="100">
       </el-table-column>
       <el-table-column prop="pageAliase" label="别名" width="120">
       </el-table-column>
-      <el-table-column prop="pageType" label="页面类型" width="150">
+      <el-table-column prop="pageType" label="页面类型" width="50">
       </el-table-column>
       <el-table-column prop="pageWebPath" label="访问路径" width="250">
       </el-table-column>
       <el-table-column prop="pagePhysicalPath" label="物理路径" width="250">
       </el-table-column>
-      <el-table-column prop="pageCreateTime" label="创建时间" width="180">
+      <el-table-column label="操作" width="90">
+        <template slot-scope="page">
+          <el-button size="small" type="text" @click="edit(page.row.pageId)">编辑</el-button>
+          <el-button size="small" type="text" @click="del(page.row.pageId)">删除</el-button>
+          <el-button size="small" type="text" @click="preview(page.row.pageId)">页面预览</el-button>
+        </template>
       </el-table-column>
     </el-table>
     <el-pagination
       layout="prev, pager, next"
-      :total="total"
+      :total=total
       :page-size="params.size"
       :current-page="params.page"
+      style="float:right;"
       @current-change="changePage"
-      style="float:right">
+    >
     </el-pagination>
   </div>
 </template>
+
 <script>
-  // 编写页面静态部分,即model及vm部分
   import * as cmsApi from '../api/cms'
-  //填写js代码，实现VM的功能，创建vue实例
+
   export default {
     data() {
       return {
         siteList: [],//站点列表
         list: [],
-        total: 50,
+        total: 0,
         params: {
           page: 1,
           size: 10,
           siteId: '',
-          pageAliase: ''
+          pageAliase: '',
         }
+
       }
     },
     methods: {
       query: function () {
         // alert('查询')
-        cmsApi.page_list(this.params.page, this.params.size,this.params).then((res) => {
+        //调用服务端的接口
+        cmsApi.page_list(this.params.page, this.params.size, this.params).then((res) => {
           //将res结果数据赋值给数据模型对象
           this.list = res.queryResult.list;
           this.total = res.queryResult.total;
         })
       },
-      changePage: function (page) {//形参就是当前页码
-        //调用query方法
-        this.params.page = page;
+      changePage: function (res) {
+        this.params.page = res;
         this.query();
+      },
+      edit: function (pageId) {
+        //打开修改页面
+        this.$router.push({
+          path: '/cms/page/edit/' + pageId
+        })
+      },
+      del: function (pageId) {
+        this.$confirm("您确认删除吗", "提示", {}).then(() => {
+          //调用服务端接口
+          cmsApi.page_del(pageId).then(res => {
+            if (res.success) {
+              this.$message.success("删除成功");
+              //刷新页面
+              this.query();
+            } else {
+              this.$message.error("删除失败")
+            }
+          })
+        });
+      },
+      preview: function (pageId) {
+        //打开浏览器窗口
+        window.open("http://www.xuehui.com/cms/preview/" + pageId);
       }
+    },
+    created() {
+      this.params.page = Number.parseInt(this.$route.query.page || 1);
+      this.params.siteId = this.$route.query.siteId || '';
     },
     mounted() {
       //当DOM元素渲染完成后调用query
@@ -96,6 +135,7 @@
     }
   }
 </script>
+
 <style>
-  /*编写页面样式,不是必须*/
+
 </style>
--- xcEduUI01/xc-ui-pc-sysmanage/src/module/home/router/index.js
+++ xcEduUI01/xc-ui-pc-sysmanage/src/module/home/router/index.js
@@ -1,15 +1,15 @@
 import Home from '@/module/home/page/home.vue';
 export default [{
-    path: '/',
-    component: Home,
-    name: '系统管理首页',
-    hidden: true
-    // ,
-    // redirect: '/home',
-    // children: [
-    //   {path: 'home', component: Home}
-    // ]
-  }/*,
+  path: '/',
+  component: Home,
+  name: '系统管理首页',
+  hidden: true
+  // ,
+  // redirect: '/home',
+  // children: [
+  //   {path: 'home', component: Home}
+  // ]
+}/*,
   {
     path: '/login',
     component: Login,
