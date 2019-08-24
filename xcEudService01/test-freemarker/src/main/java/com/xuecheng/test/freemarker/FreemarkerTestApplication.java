20a4e5f8276950507e36aeb5e4bb67e585e95e97
lyz8jj0 <844216765@qq.com>
2019-08-24 11:01:15 +0800

    freemarker测试项目搭建成功

---
+++ xcEudService01/test-freemarker/src/main/java/com/xuecheng/test/freemarker/FreemarkerTestApplication.java
@@ -0,0 +1,15 @@
+package com.xuecheng.test.freemarker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by 李新宇
+ * 2019-08-24 10:45
+ */
+@SpringBootApplication
+public class FreemarkerTestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FreemarkerTestApplication.class, args);
+    }
+}
---
+++ xcEudService01/test-freemarker/src/main/resources/templates/test1.ftl
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <meta charset="utf‐8">
+
+    <title>Hello World!</title></head>
+<body>
+Hello ${name}!
+
+</body>
+</html>
