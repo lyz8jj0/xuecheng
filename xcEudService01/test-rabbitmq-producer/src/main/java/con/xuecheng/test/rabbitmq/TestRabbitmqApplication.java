df8a75b249356f38d0787e9382359ca6bd03eb85
lyz8jj0 <844216765@qq.com>
2019-08-28 11:54:04 +0800

    RabbitMQ研究-与springboot整合-搭建环境

---
+++ xcEudService01/test-rabbitmq-producer/src/main/java/con/xuecheng/test/rabbitmq/TestRabbitmqApplication.java
@@ -0,0 +1,15 @@
+package con.xuecheng.test.rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by 李新宇
+ * 2019-08-28 11:52
+ */
+@SpringBootApplication
+public class TestRabbitmqApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TestRabbitmqApplication.class, args);
+    }
+}
