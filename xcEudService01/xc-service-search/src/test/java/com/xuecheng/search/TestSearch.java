17438417d187d3f0f9627fa73216262ec9eed64e
lyz8jj0 <844216765@qq.com>
2019-08-30 09:58:21 +0800

    ElasticSearch-搜索管理-MatchQuery

--- xcEudService01/xc-service-search/src/test/java/com/xuecheng/search/TestSearch.java
+++ xcEudService01/xc-service-search/src/test/java/com/xuecheng/search/TestSearch.java
@@ -229,6 +229,48 @@ public class TestSearch {
             Date timestamp = dateFormat.parse((String) sourceAsMap.get("timestamp"));
         }
     }
+
+
+    //根据id查询
+    @Test
+    public void testMatchQuery() throws ParseException, IOException {
+        //搜索请求对象
+        SearchRequest searchRequest = new SearchRequest("xc_course");
+        //指定类型
+        searchRequest.types("doc");
+        //搜索源构建对象
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+
+        searchSourceBuilder.query(QueryBuilders.matchQuery("description", "spring开发框架")
+                .minimumShouldMatch("80%"));
+        //设置源字段过虑, 第一个参数结果集包括哪些字段, 第二个参数表示结果集不抱括哪些字段
+        searchSourceBuilder.fetchSource(new String[]{"name", "studymodel", "price", "timestamp"}, new String[]{});
+        //向搜索请求对象中设置搜索源
+        searchRequest.source(searchSourceBuilder);
+        //执行搜索,向es发起请求
+        SearchResponse searchResponse = client.search(searchRequest);
+        //搜索结果
+        SearchHits hits = searchResponse.getHits();
+        //匹配的总记录数
+        long totalHits = hits.getTotalHits();
+        //得到匹配度高的文档
+        SearchHit[] searchHits = hits.getHits();
+        //日期格式化对象
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        for (SearchHit hit : searchHits) {
+            //文档的主键
+            String id = hit.getId();
+            //源文档内容
+            Map<String, Object> sourceAsMap = hit.getSourceAsMap();
+            String name = (String) sourceAsMap.get("name");
+            //学习模式
+            String studymodel = (String) sourceAsMap.get("studymodel");
+            //价格
+            Double price = (Double) sourceAsMap.get("price");
+
+            Date timestamp = dateFormat.parse((String) sourceAsMap.get("timestamp"));
+        }
+    }
 }
 
 
