5466def4325be3ea8dd0eeb70ec063691adc88a8
lyz8jj0 <844216765@qq.com>
2019-09-02 15:36:33 +0800

    分布式文件系统研究-测试-上传文件测试

--- xcEduService01/test-fastdfs/src/test/java/com/xuecheng/test/fastdfs/TestFastDFS.java
+++ xcEduService01/test-fastdfs/src/test/java/com/xuecheng/test/fastdfs/TestFastDFS.java
@@ -1,5 +1,7 @@
 package com.xuecheng.test.fastdfs;
 
+import org.csource.fastdfs.*;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -13,6 +15,30 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class TestFastDFS {
 
     //上传文件
+    @Test
+    public void testUpload() {
+        try {
+            //加载fastfds-client.properties配置文件
+            ClientGlobal.initByProperties("config/fastdfs-client.properties");
+            //定义TrackerClient, 用于请求TrackerServer
+            TrackerClient trackerClient = new TrackerClient();
+            //连接tracker
+            TrackerServer trackerServer = trackerClient.getConnection();
+            //获取Storage
+            StorageServer storeStorage = trackerClient.getStoreStorage(trackerServer);
+            //创建StorageClient
+            StorageClient1 storageClient1 = new StorageClient1(trackerServer, storeStorage);
+            //向storage服务器上传文件
+            //本地文件的路径
+            String filePath = "/Users/lixinyu/Desktop/qq.png";
+            //上传成功后拿到id
+            String fileId = storageClient1.upload_file1(filePath, "png", null);
+            System.out.println(fileId);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
 
     //下载文件
 }
