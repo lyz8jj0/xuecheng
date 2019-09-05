96c7d82ba64751a7637e566ff02ab6b2579cb9ba
lyz8jj0 <844216765@qq.com>
2019-09-05 11:25:40 +0800

    Feign远程调用-Feign测试

---
+++ xcEduService01/xc-service-manage-course/src/main/java/com/xuecheng/manage_course/client/CmsPageClient.java
@@ -0,0 +1,19 @@
+package com.xuecheng.manage_course.client;
+
+import com.xuecheng.framework.domain.cms.CmsPage;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * Created by 李新宇
+ * 2019-09-05 11:15
+ */
+@FeignClient(value = "XC-SERVICE-MANAGE-CMS")
+public interface CmsPageClient {
+
+    //根据页面id查询页面信息, 远程调用cms请求数据
+    @GetMapping("/cms/page/get/{id}") //用GetMapping标识远程调用的http的方法类型
+    public CmsPage findCmsPageById(@PathVariable("id") String id);
+
+}
