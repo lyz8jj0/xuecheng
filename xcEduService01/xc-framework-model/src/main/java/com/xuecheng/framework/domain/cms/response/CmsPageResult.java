c6badc00a1537af005bbfd9549659b973d8329ad
lyz8jj0 <844216765@qq.com>
2019-09-28 17:59:47 +0800

    CmsPageResult返回结构类型增加无参构造方法

--- xcEduService01/xc-framework-model/src/main/java/com/xuecheng/framework/domain/cms/response/CmsPageResult.java
+++ xcEduService01/xc-framework-model/src/main/java/com/xuecheng/framework/domain/cms/response/CmsPageResult.java
@@ -4,11 +4,15 @@ import com.xuecheng.framework.domain.cms.CmsPage;
 import com.xuecheng.framework.model.response.ResponseResult;
 import com.xuecheng.framework.model.response.ResultCode;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 /**
  * Created by mrt on 2018/3/31.
  */
 @Data
+@NoArgsConstructor
+@ToString
 public class CmsPageResult extends ResponseResult {
 
     CmsPage cmsPage;
