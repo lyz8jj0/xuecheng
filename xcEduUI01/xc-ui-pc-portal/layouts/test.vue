e32822645ed03daac99d63a1f3f8847d685770dd
lyz8jj0 <844216765@qq.com>
2019-10-11 10:57:43 +0800

    Nuxts.js基本使用-获取数据-async&await

---
+++ xcEduUI01/xc-ui-pc-portal/layouts/test.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    这里是头部区域
+    <nuxt/>
+    这里 尾部区域
+  </div>
+</template>
+<script>
+  import Footer from '../components/Footer.vue'
+  import Header from '../components/Header.vue'
+
+  export default {}
+</script>
+<style>
+
+</style>
