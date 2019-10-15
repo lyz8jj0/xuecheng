cc79a01e738c9dbe85a8071ee00770d4b993223d
lyz8jj0 <844216765@qq.com>
2019-10-15 19:11:14 +0800

    认证接口开发-service开发

--- xcEduService01/xc-service-api/src/main/java/com/xuecheng/api/auth/AuthControllerApi.java
+++ xcEduService01/xc-service-api/src/main/java/com/xuecheng/api/auth/AuthControllerApi.java
@@ -1,6 +1,7 @@
 package com.xuecheng.api.auth;
 
 import com.xuecheng.framework.domain.ucenter.request.LoginRequest;
+import com.xuecheng.framework.domain.ucenter.response.LoginResult;
 import com.xuecheng.framework.model.response.ResponseResult;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -12,7 +13,7 @@ import io.swagger.annotations.ApiOperation;
 @Api(value = "用户认证",description = "用户认证接口")
 public interface AuthControllerApi {
     @ApiOperation("登录")
-    public LoginRequest login(LoginRequest loginRequest);
+    public LoginResult login(LoginRequest loginRequest);
 
     @ApiOperation("退出")
     public ResponseResult logout();
