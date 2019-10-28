45d1b1133c39cdddf5c80272aa9abd140c4d04cd
lyz8jj0 <844216765@qq.com>
2019-10-28 17:50:04 +0800

    媒资管理-上传文件-Api接口和上传注册完成

---
+++ xcEduService01/xc-service-manage-media/src/main/java/com/xuecheng/manage_media/controller/MediaUploadController.java
@@ -0,0 +1,46 @@
+package com.xuecheng.manage_media.controller;
+
+import com.xuecheng.api.media.MediaUploadControllerApi;
+import com.xuecheng.framework.domain.media.response.CheckChunkResult;
+import com.xuecheng.framework.model.response.ResponseResult;
+import com.xuecheng.manage_media.service.MediaUploadService;
+import jdk.management.resource.ResourceRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * Created by 李新宇
+ * 2019-10-28 17:14
+ */
+@RestController
+@RequestMapping("/media/upload")
+public class MediaUploadController implements MediaUploadControllerApi {
+
+    @Autowired
+    MediaUploadService mediaUploadService;
+
+    //文件上传前的注册
+    @Override
+    @PostMapping("/register")
+    public ResponseResult register(String fileMd5, String fileName, Long fileSize, String mimetype, String fileExt) {
+        return mediaUploadService.register(fileMd5,fileName,fileSize,mimetype,fileExt);
+    }
+
+    @Override
+    public CheckChunkResult checkchunk(String fileMd5, Integer chunk, Integer chunkSize) {
+        return null;
+    }
+
+    @Override
+    public ResponseResult uploadchunk(MultipartFile file, String fileMd5, Integer chunk) {
+        return null;
+    }
+
+    @Override
+    public ResourceRequest mergechunks(String fileMd5, String fileName, Long fileSize, String mimetype, String fileExt) {
+        return null;
+    }
+}
