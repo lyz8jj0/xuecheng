11090b6807c12a8a753fb5d342d08d3d4af58513
lyz8jj0 <844216765@qq.com>
2019-10-29 15:55:41 +0800

    视频处理-视频处理实现-生成m3u8

--- xcEduService01/xc-service-manage-media-processor/src/main/java/com/xuecheng/manage_media_process/mq/MediaProcessTask.java
+++ xcEduService01/xc-service-manage-media-processor/src/main/java/com/xuecheng/manage_media_process/mq/MediaProcessTask.java
@@ -2,7 +2,9 @@ package com.xuecheng.manage_media_process.mq;
 
 import com.alibaba.fastjson.JSON;
 import com.xuecheng.framework.domain.media.MediaFile;
+import com.xuecheng.framework.domain.media.MediaFileProcess;
 import com.xuecheng.framework.domain.media.MediaFileProcess_m3u8;
+import com.xuecheng.framework.utils.HlsVideoUtil;
 import com.xuecheng.framework.utils.Mp4VideoUtil;
 import com.xuecheng.manage_media_process.dao.MediaFileRepository;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -10,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
@@ -74,7 +77,40 @@ public class MediaProcessTask {
             mediaFileRepository.save(mediaFile);
             return;
         }
-
         //4. 将mp4生成m3u8和ts
+        //String ffmpeg_path, String video_path, String m3u8_name,String m3u8folder_path
+        //mp4视频文件路径
+        String mp4_video_path = serverPath + mediaFile.getFilePath() + mp4_name;
+        //m3u8_name文件名称
+        String m3u8_name = mediaFile.getFileId() + ".m3u8";
+        //m3u8文件所在目录
+        String m3u8folder_path = serverPath + mediaFile.getFileId() + "hls/";
+        HlsVideoUtil hlsVideoUtil = new HlsVideoUtil(ffmpeg_path, mp4_video_path, m3u8_name, m3u8folder_path);
+        //生成m3u8和ts文件
+        String tsResult = hlsVideoUtil.generateM3u8();
+        if (tsResult == null || !tsResult.equals("success")) {
+            //处理失败
+            mediaFile.setProcessStatus("303003");
+            //定义mediaFileprocess_m3u8
+            MediaFileProcess_m3u8 mediaFileProcess_m3u8 = new MediaFileProcess_m3u8();
+            //记录失败原因
+            mediaFileProcess_m3u8.setErrormsg(result);
+            mediaFile.setMediaFileProcess_m3u8(mediaFileProcess_m3u8);
+            mediaFileRepository.save(mediaFile);
+            return;
+        }
+        //处理成功
+        //获取ts文件列表
+        List<String> ts_list = hlsVideoUtil.get_ts_list();
+        mediaFile.setProcessStatus("303002");
+        //定义mediaFileProcess_m3u8
+        MediaFileProcess_m3u8 mediaFileProcess_m3u8 = new MediaFileProcess_m3u8();
+        mediaFileProcess_m3u8.setTslist(ts_list);
+        mediaFile.setMediaFileProcess_m3u8(mediaFileProcess_m3u8);
+
+        //保存fileUrl(此url就是视频播放的相对路径)
+        String fileUrl = mediaFile.getFilePath() + "hls/" + m3u8_name;
+        mediaFile.setFileUrl(fileUrl);
+        mediaFileRepository.save(mediaFile);
     }
 }
