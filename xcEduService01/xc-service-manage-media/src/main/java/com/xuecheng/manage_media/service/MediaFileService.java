e49a839eb5d3af4fe7ecf1ef2bb57a8f4a5fb31c
lyz8jj0 <844216765@qq.com>
2019-10-30 14:53:11 +0800

    我的媒资-服务端开发

---
+++ xcEduService01/xc-service-manage-media/src/main/java/com/xuecheng/manage_media/service/MediaFileService.java
@@ -0,0 +1,83 @@
+package com.xuecheng.manage_media.service;
+
+import com.alibaba.fastjson.JSON;
+import com.xuecheng.framework.domain.media.MediaFile;
+import com.xuecheng.framework.domain.media.request.QueryMediaFileRequest;
+import com.xuecheng.framework.domain.media.response.CheckChunkResult;
+import com.xuecheng.framework.domain.media.response.MediaCode;
+import com.xuecheng.framework.exception.ExceptionCast;
+import com.xuecheng.framework.model.response.CommonCode;
+import com.xuecheng.framework.model.response.QueryResponseResult;
+import com.xuecheng.framework.model.response.QueryResult;
+import com.xuecheng.framework.model.response.ResponseResult;
+import com.xuecheng.manage_media.config.RabbitMQConfig;
+import com.xuecheng.manage_media.dao.MediaFileRepository;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.*;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * Created by 李新宇
+ * 2019-10-28 17:16
+ */
+@Service
+public class MediaFileService {
+
+    @Autowired
+    MediaFileRepository mediaFileRepository;
+
+    //查询我的媒资列表
+    public QueryResponseResult<MediaFile> findList(int page, int size, QueryMediaFileRequest queryMediaFileRequest) {
+        if (queryMediaFileRequest == null) {
+            queryMediaFileRequest = new QueryMediaFileRequest();
+        }
+        //条件值对象
+        MediaFile mediaFile = new MediaFile();
+        if (StringUtils.isNotEmpty(queryMediaFileRequest.getTag())) {
+            mediaFile.setTag(queryMediaFileRequest.getTag());
+        }
+        if (StringUtils.isNotEmpty(queryMediaFileRequest.getFileOriginalName())) {
+            mediaFile.setFileOriginalName(queryMediaFileRequest.getFileOriginalName());
+        }
+        if (StringUtils.isNotEmpty(queryMediaFileRequest.getProcessStatus())) {
+            mediaFile.setProcessStatus(queryMediaFileRequest.getProcessStatus());
+        }
+        //条件匹配器
+        ExampleMatcher exampleMatcher = ExampleMatcher.matching()
+                .withMatcher("tag", ExampleMatcher.GenericPropertyMatchers.contains())
+                .withMatcher("fileOriginName", ExampleMatcher.GenericPropertyMatchers.contains());
+        //定义example条件对象
+        Example<MediaFile> example = Example.of(mediaFile, exampleMatcher);
+        //分页查询对象
+        if (page <= 0) {
+            page = 1;
+        }
+        page = page - 1;
+        if (size <= 0) {
+            size = 10;
+        }
+        Pageable pageable = new PageRequest(page, size);
+        //分页查询
+        Page<MediaFile> all = mediaFileRepository.findAll(example, pageable);
+        //总记录数
+        long total = all.getTotalElements();
+        //数据列表
+        List<MediaFile> content = all.getContent();
+        //返回数据集
+        QueryResult<MediaFile> queryResult = new QueryResult<>();
+        queryResult.setList(content);
+        queryResult.setTotal(total);
+
+        QueryResponseResult queryResponseResult = new QueryResponseResult(CommonCode.SUCCESS, queryResult);
+        return queryResponseResult;
+    }
+}
