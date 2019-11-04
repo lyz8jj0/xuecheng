3cb8b7709d556640d97583b9096fc4def7e4a198
lyz8jj0 <844216765@qq.com>
2019-11-04 18:09:48 +0800

    用户认证配置

--- xcEduService01/xc-service-learning/src/main/java/com/xuecheng/learning/controller/CourseLearningController.java
+++ xcEduService01/xc-service-learning/src/main/java/com/xuecheng/learning/controller/CourseLearningController.java
@@ -6,6 +6,7 @@ import com.xuecheng.learning.service.LearningService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  * 2019-11-01 10:44
  */
 @RestController
+@RequestMapping("/learning/course")
 public class CourseLearningController implements CourseLearningControllerApi {
 
     @Autowired
--- xcEduService01/xc-service-ucenter-auth/src/test/java/com/xuecheng/auth/TestJwt.java
+++ xcEduService01/xc-service-ucenter-auth/src/test/java/com/xuecheng/auth/TestJwt.java
@@ -11,7 +11,7 @@ import org.springframework.test.context.junit4.SpringRunner;
  */
 @SpringBootTest
 @RunWith(SpringRunner.class)
-public class TestJwt {
+public class  TestJwt {
 
 
     //创建jwt令牌
