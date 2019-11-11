9096adeac14be4b610fa31747a645c1592067189
lyz8jj0 <844216765@qq.com>
2019-11-11 15:12:09 +0800

    身份校验-身份校验过虑器编写

--- xcEduService01/xc-govern-gateway/src/main/java/com/xuecheng/govern/gateway/filter/LoginFilter.java
+++ xcEduService01/xc-govern-gateway/src/main/java/com/xuecheng/govern/gateway/filter/LoginFilter.java
@@ -6,18 +6,26 @@ import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
 import com.xuecheng.framework.model.response.CommonCode;
 import com.xuecheng.framework.model.response.ResponseResult;
+import com.xuecheng.govern.gateway.service.AuthService;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
+ * 身份校验过滤器
+ * <p>
  * Created by 李新宇
  * 2019-11-11 09:44
  */
-//@Component
-public class LoginFilterTest extends ZuulFilter {
+
+@Component
+public class LoginFilter extends ZuulFilter {
+
+    @Autowired
+    AuthService authService;
 
     @Override
     public String filterType() {
@@ -54,24 +62,44 @@ public class LoginFilterTest extends ZuulFilter {
         HttpServletRequest request = requestContext.getRequest();
         //得到响应
         HttpServletResponse response = requestContext.getResponse();
-        //得到Authorization头
-        String authorization = request.getHeader("Authorization");
-        if (StringUtils.isEmpty(authorization)) {
+        //取出cookie中的身份令牌
+        String tokenFromCookie = authService.getTokenFromCookie(request);
+        if (StringUtils.isEmpty(tokenFromCookie)) {
             //拒绝访问
-            requestContext.setSendZuulResponse(false);
-            //设置响应代码
-            requestContext.setResponseStatusCode(200);
-            //构建响应的信息
-            ResponseResult responseResult = new ResponseResult(CommonCode.UNAUTHENTICATED);
-            //转成json
-            String jsonString = JSON.toJSONString(responseResult);
-            requestContext.setResponseBody(jsonString);
-            //转成json, 设置contentType
-            response.setContentType("application/json;charset=utf-8");
-            return null;
+            access_denied();
         }
-
-
+        //从header中取jwt
+        String jwtFromHeader = authService.getJwtFromHeader(request);
+        if (StringUtils.isEmpty(jwtFromHeader)) {
+            //拒绝访问
+            access_denied();
+        }
+        //从redis取出jwt过期时间
+        long expire = authService.getExpire(tokenFromCookie);
+        if (expire < 0) {
+            //拒绝访问
+            access_denied();
+        }
         return null;
     }
+
+    //拒绝访问
+    private void access_denied() {
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        //得到响应
+        HttpServletResponse response = requestContext.getResponse();
+        //拒绝访问
+        requestContext.setSendZuulResponse(false);
+        //设置响应代码
+        requestContext.setResponseStatusCode(200);
+        //构建响应的信息
+        ResponseResult responseResult = new ResponseResult(CommonCode.UNAUTHENTICATED);
+        //转成json
+        String jsonString = JSON.toJSONString(responseResult);
+        requestContext.setResponseBody(jsonString);
+        //转成json, 设置contentType
+        response.setContentType("application/json;charset=utf-8");
+    }
+
+
 }
