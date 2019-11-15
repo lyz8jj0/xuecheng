4819f3f672d0a3d4fe03cfca05bd0dfd06de3eb0
lyz8jj0 <844216765@qq.com>
2019-11-15 15:24:21 +0800

    动态查询用户的权限-认证服务查询用户权限

--- xcEduService01/xc-service-ucenter-auth/src/main/java/com/xuecheng/auth/service/UserDetailsServiceImpl.java
+++ xcEduService01/xc-service-ucenter-auth/src/main/java/com/xuecheng/auth/service/UserDetailsServiceImpl.java
@@ -53,7 +53,9 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 //        XcUserExt userext = new XcUserExt();
 //        userext.setUsername("itcast");
 //        userext.setPassword(new BCryptPasswordEncoder().encode("123"));
-        userext.setPermissions(new ArrayList<XcMenu>());//权限暂时先用静态的
+//        userext.setPermissions(new ArrayList<XcMenu>());//权限暂时先用静态的
+
+
         //取出正确密码（hash值）
         String password = userext.getPassword();
         //这里暂时使用静态密码
@@ -61,10 +63,14 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         //用户权限，这里暂时使用静态数据，最终会从数据库读取
         //从数据库获取权限
         List<XcMenu> permissions = userext.getPermissions();
+        if (permissions == null) {
+            permissions = new ArrayList<>();
+        }
         List<String> user_permission = new ArrayList<>();
         permissions.forEach(item-> user_permission.add(item.getCode()));
-        user_permission.add("course_get_baseinfo");//查询课程信息
-        user_permission.add("course_pic_list ");//图片查询
+        //使用静态的权限表示用户所拥有的权限
+//        user_permission.add("course_get_baseinfo");//查询课程信息
+//        user_permission.add("course_pic_list ");//图片查询
         String user_permission_string  = StringUtils.join(user_permission.toArray(), ",");
         UserJwt userDetails = new UserJwt(username,
                 password,
